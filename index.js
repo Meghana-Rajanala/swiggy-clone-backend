5ef46e0df4f3b52019a86bf93a9d4898dcc80eb4
Meghana-Rajanala <121629205+Meghana-Rajanala@users.noreply.github.com>
2024-05-16 17:54:35 +0530

    update home send in index.js

--- index.js
+++ index.js
@@ -33,7 +33,13 @@ app.use('/uploads',express.static('uploads'));
 
 
 //mangodb connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 30000, // 30 seconds timeout
+      }
+)
 .then(()=>{console.log("MongoDB connected")})
 .catch((err)=>{console.log(err)})
 
